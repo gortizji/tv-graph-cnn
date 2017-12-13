Better visualization in TensorBoard. Solved bug in computation of the metric for test data (mean of means is not the same as the mean of the dataset).

signal_classification/models.py
@@ -19,13 +19,6 @@ def _bias_variable(shape):
 def _variable_summaries(var):
     """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
     with tf.name_scope('summaries'):
-        mean = tf.reduce_mean(var)
-        tf.summary.scalar('mean', mean)
-        with tf.name_scope('stddev'):
-            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-        tf.summary.scalar('stddev', stddev)
-        tf.summary.scalar('max', tf.reduce_max(var))
-        tf.summary.scalar('min', tf.reduce_min(var))
         tf.summary.histogram('histogram', var)
 
 
@@ -67,7 +60,7 @@ def deep_fir_tv_fc_fn(x, L, num_classes, time_filter_orders, vertex_filter_order
         "Filter parameters should all be of the same length"
 
     n_layers = len(time_filter_orders)
-    phase = tf.placeholder(tf.bool)
+    phase = tf.placeholder(tf.bool, name="phase")
 
     # Convolutional layers
     vpool = x
@@ -75,7 +68,7 @@ def deep_fir_tv_fc_fn(x, L, num_classes, time_filter_orders, vertex_filter_order
         with tf.name_scope("conv%d" % n):
             conv = _fir_tv_layer(vpool, L[n], time_filter_orders[n], vertex_filter_orders[n], num_filters[n])
             conv = _batch_normalization(conv, is_training=phase, scope="conv%d" % n)
-            conv = tf.nn.relu(conv)
+            conv = tf.nn.relu(conv, name="conv%d" % n)
 
         with tf.name_scope("subsampling%d" % n):
             tpool = tf.layers.max_pooling2d(
@@ -104,6 +97,7 @@ def deep_fir_tv_fc_fn(x, L, num_classes, time_filter_orders, vertex_filter_order
             activation=None,
             use_bias=True,
         )
+        fc = tf.identity(fc, name="fc")
     return fc, phase
 
 
@@ -112,7 +106,7 @@ def deep_cheb_fc_fn(x, L, num_classes, vertex_filter_orders, num_filters, vertex
         "Filter parameters should all be of the same length"
 
     n_layers = len(vertex_filter_orders)
-    phase = tf.placeholder(tf.bool)
+    phase = tf.placeholder(tf.bool, name="phase")
 
     # Convolutional layers
     vpool = x
@@ -120,7 +114,7 @@ def deep_cheb_fc_fn(x, L, num_classes, vertex_filter_orders, num_filters, vertex
         with tf.name_scope("conv%d" % n):
             conv = _cheb_conv_layer(vpool, L[n], vertex_filter_orders[n], num_filters[n])
             conv = _batch_normalization(conv, is_training=phase, scope="conv%d" % n)
-            conv = tf.nn.relu(conv)
+            conv = tf.nn.relu(conv, name="conv%d" % n)
 
         with tf.name_scope("vertex_pooling%d" % n):
             if vertex_poolings[n] > 1:
@@ -133,7 +127,6 @@ def deep_cheb_fc_fn(x, L, num_classes, vertex_filter_orders, num_filters, vertex
             else:
                 vpool = conv
 
-
     # Last fully connected layer
     with tf.name_scope("fc"):
         fc_input = tf.layers.flatten(vpool)
@@ -141,8 +134,10 @@ def deep_cheb_fc_fn(x, L, num_classes, vertex_filter_orders, num_filters, vertex
             inputs=fc_input,
             units=num_classes,
             activation=None,
-            use_bias=True,
+            use_bias=True
         )
+
+        fc = tf.identity(fc, "fc")
     return fc, phase
 
 

signal_classification/test.py
@@ -72,8 +72,8 @@ def run_training(L, train_mb_source, test_mb_source):
 
     # Create data placeholders
     num_vertices, _ = L[0].get_shape()
-    x = tf.placeholder(tf.float32, [None, num_vertices, FLAGS.num_frames, 1])
-    y_ = tf.placeholder(tf.float32, [None, FLAGS.num_classes])
+    x = tf.placeholder(tf.float32, [None, num_vertices, FLAGS.num_frames, 1], name="x")
+    y_ = tf.placeholder(tf.float32, [None, FLAGS.num_classes], name="labels")
 
     # Initialize model
     if FLAGS.model_type == "deep_fir":
@@ -86,7 +86,7 @@ def run_training(L, train_mb_source, test_mb_source):
                                           num_filters=FLAGS.num_filters,
                                           time_poolings=FLAGS.time_poolings,
                                           vertex_poolings=FLAGS.vertex_poolings)
-        dropout = tf.placeholder(tf.float32)
+        dropout = tf.placeholder(tf.float32, name="keep_prob")
     elif FLAGS.model_type == "deep_cheb":
         print("Training deep Chebyshev time invariant model...")
         xt = tf.transpose(x, perm=[0, 1, 3, 2])
@@ -96,12 +96,12 @@ def run_training(L, train_mb_source, test_mb_source):
                                         vertex_filter_orders=FLAGS.vertex_filter_orders,
                                         num_filters=FLAGS.num_filters,
                                         vertex_poolings=FLAGS.vertex_poolings)
-        dropout = tf.placeholder(tf.float32)
+        dropout = tf.placeholder(tf.float32, name="keep_prob")
     elif FLAGS.model_type == "fc":
         print("Training linear classifier model...")
         logits = fc_fn(x, FLAGS.num_classes)
-        dropout = tf.placeholder(tf.float32)
-        phase = tf.placeholder(tf.bool)
+        dropout = tf.placeholder(tf.float32, name="keep_prob")
+        phase = tf.placeholder(tf.bool, name="phase")
     else:
         raise ValueError("model_type not valid.")
 
@@ -114,8 +114,8 @@ def run_training(L, train_mb_source, test_mb_source):
         # Define metric
     with tf.name_scope("metric"):
         correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y_, 1))
-        correct_prediction = tf.cast(correct_prediction, tf.float32)
-        accuracy = tf.reduce_mean(correct_prediction)
+        correct_prediction = tf.cast(correct_prediction, tf.float32, name="correct_prediction")
+        accuracy = tf.reduce_mean(correct_prediction, name="accuracy")
         tf.summary.scalar('accuracy', accuracy)
 
     extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
@@ -175,45 +175,93 @@ def run_training(L, train_mb_source, test_mb_source):
             # Save a checkpoint and evaluate the model periodically.
             if (step + 1) % (FLAGS.num_train // FLAGS.batch_size) == 0 or (step + 1) == MAX_STEPS or (
                     step + 1) % 30 == 0:
-                checkpoint_file = os.path.join(FLAGS.log_dir, 'model.ckpt')
+                checkpoint_file = os.path.join(FLAGS.log_dir, 'model')
                 saver.save(sess, checkpoint_file, global_step=step)
 
-                still_data = True
-                test_accuracies = []
-                test_mb_source.restart()
-                while still_data:
-                    test_feed_dict, still_data = _fill_feed_dict(test_mb_source, x, y_, dropout, phase, False)
-                    test_accuracies.append(sess.run(accuracy, feed_dict=test_feed_dict))
+                test_accuracy = _eval_metric(sess, correct_prediction, dropout, phase, x, y_, test_mb_source)
 
                 print("--------------------")
-                print('Test accuracy = %.2f' % np.mean(test_accuracies))
+                print('Test accuracy = %.2f' % test_accuracy)
                 if (step + 1) % (FLAGS.num_train // FLAGS.batch_size) == 0:
                     print("====================")
                 else:
                     print("--------------------")
 
 
+def _eval_metric(sess, correct_prediction, dropout, phase, x, y, test_mb_source,):
+    still_data = True
+    test_correct_predictions = []
+    test_mb_source.restart()
+    while still_data:
+        test_feed_dict, still_data = _fill_feed_dict(test_mb_source, x, y, dropout, phase, False)
+        test_correct_predictions.append(sess.run(correct_prediction, feed_dict=test_feed_dict))
+
+    return np.mean(test_correct_predictions)
+
+
+def run_eval(test_mb_source):
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(os.path.join(FLAGS.log_dir, "model-999.meta"))
+        saver.restore(sess, tf.train.latest_checkpoint(FLAGS.log_dir))
+        graph = tf.get_default_graph()
+
+        # Get inputs
+        x = graph.get_tensor_by_name("x:0")
+        y = graph.get_tensor_by_name("labels:0")
+        keep_prob = graph.get_tensor_by_name("keep_prob:0")
+        phase = graph.get_tensor_by_name("phase:0")
+
+        # Get output
+        correct_prediction = graph.get_tensor_by_name("metric/correct_prediction:0")
+
+        print("Evaluation accuracy: %.2f" % _eval_metric(sess, correct_prediction, keep_prob, phase, x, y, test_mb_source))
+
+
 def main(_):
     # Initialize tempdir
-    if tf.gfile.Exists(FLAGS.log_dir):
-        tf.gfile.DeleteRecursively(FLAGS.log_dir)
-    tf.gfile.MakeDirs(FLAGS.log_dir)
+    FLAGS.log_dir = os.path.join(FLAGS.log_dir, FLAGS.model_type)
+    exp_n = _last_exp(FLAGS.log_dir) + 1 if FLAGS.action == "train" else _last_exp(FLAGS.log_dir)
+    FLAGS.log_dir = os.path.join(FLAGS.log_dir, "exp_" + str(exp_n))
+    print(FLAGS.log_dir)
 
-    # Initialize data
-    G = graphs.Community(FLAGS.num_vertices)
-    G.compute_laplacian("normalized")
+    if FLAGS.action == "train":
+        if tf.gfile.Exists(FLAGS.log_dir):
+            tf.gfile.DeleteRecursively(FLAGS.log_dir)
+        tf.gfile.MakeDirs(FLAGS.log_dir)
 
-    train_data, train_labels = generate_spectral_samples(
-        N=FLAGS.num_train // 4,
-        G=G,
-        T=FLAGS.num_frames,
-        f_h=FLAGS.f_h,
-        f_l=FLAGS.f_h,
-        lambda_h=FLAGS.lambda_h,
-        lambda_l=FLAGS.lambda_l,
-        sigma=FLAGS.sigma,
-        sigma_n=FLAGS.sigma_n
-    )
+        # Initialize data
+        G = graphs.Community(FLAGS.num_vertices, seed=FLAGS.seed)
+        G.compute_laplacian("normalized")
+        # Save graph
+        np.save(os.path.join(FLAGS.log_dir, "graph_weights"), G.W.todense())
+
+        # Prepare pooling
+        num_levels = _number_of_pooling_levels(FLAGS.vertex_poolings)
+        adjacencies, perm = coarsen(G.A, levels=num_levels)  # Coarsens in powers of 2
+        np.save(os.path.join(FLAGS.log_dir, "ordering"), perm)
+        L = [initialize_laplacian_tensor(A) for A in adjacencies]
+        L = keep_pooling_laplacians(L, FLAGS.vertex_poolings)
+
+    elif FLAGS.action == "eval":
+        W = np.load(os.path.join(FLAGS.log_dir, "graph_weights.npy"))
+        G = graphs.Graph(W)
+        G.compute_laplacian("normalized")
+        perm = np.load(os.path.join(FLAGS.log_dir, "ordering.npy"))
+
+    if FLAGS.action == "train":
+        train_data, train_labels = generate_spectral_samples(
+            N=FLAGS.num_train // 4,
+            G=G,
+            T=FLAGS.num_frames,
+            f_h=FLAGS.f_h,
+            f_l=FLAGS.f_h,
+            lambda_h=FLAGS.lambda_h,
+            lambda_l=FLAGS.lambda_l,
+            sigma=FLAGS.sigma,
+            sigma_n=FLAGS.sigma_n
+        )
+        train_data = perm_data(train_data, perm)
+        train_mb_source = TemporalGraphBatchSource(train_data, train_labels, repeat=True)
 
     test_data, test_labels = generate_spectral_samples(
         N=FLAGS.num_test // 4,
@@ -227,20 +275,18 @@ def main(_):
         sigma_n=FLAGS.sigma_n
     )
 
-    # Prepare pooling
-    num_levels = _number_of_pooling_levels(FLAGS.vertex_poolings)
-    adjacencies, perm = coarsen(G.A, levels=num_levels)  # Coarsens in powers of 2
-    L = [initialize_laplacian_tensor(A) for A in adjacencies]
-    L = keep_pooling_laplacians(L, FLAGS.vertex_poolings)
-    train_data = perm_data(train_data, perm)
     test_data = perm_data(test_data, perm)
-
-    # Initialize minibatch sources
-    train_mb_source = TemporalGraphBatchSource(train_data, train_labels, repeat=True)
     test_mb_source = TemporalGraphBatchSource(test_data, test_labels, repeat=False)
 
-    # Run training and evaluation loop
-    run_training(L, train_mb_source, test_mb_source)
+    if FLAGS.action == "train":
+        # Run training and evaluation loop
+        print("Training model...")
+        run_training(L, train_mb_source, test_mb_source)
+    elif FLAGS.action == "eval":
+        print("Evaluating model...")
+        run_eval(test_mb_source)
+    else:
+        raise ValueError("No valid action selected")
 
 
 def _number_of_pooling_levels(vertex_poolings):
@@ -249,6 +295,18 @@ def _number_of_pooling_levels(vertex_poolings):
 
 def _number_of_trainable_params():
     return np.sum([np.product(x.shape) for x in tf.trainable_variables()])
+
+
+def _last_exp(log_dir):
+    exp_numbers = []
+    if not os.path.exists(log_dir):
+        return 0
+    for file in os.listdir(log_dir):
+        if "exp" not in file:
+            continue
+        else:
+            exp_numbers.append(int(file.split("_")[1]))
+    return max(exp_numbers) if len(exp_numbers) > 0 else 0
 
 
 if __name__ == '__main__':
@@ -260,6 +318,12 @@ if __name__ == '__main__':
         help="Model type"
     )
     parser.add_argument(
+        "--action",
+        type=str,
+        default="train",
+        help="Action to perform on the model"
+    )
+    parser.add_argument(
         '--learning_rate',
         type=float,
         default=1e-4,
@@ -268,7 +332,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--num_epochs',
         type=int,
-        default=20,
+        default=10,
         help='Number of epochs to run trainer.'
     )
     parser.add_argument(
@@ -292,8 +356,13 @@ if __name__ == '__main__':
     parser.add_argument(
         '--log_dir',
         type=str,
-        default=os.path.join(TEMPDIR, "tensorflow/tv_graph_cnn/logs/cheb_net"),
+        default=os.path.join(TEMPDIR, "tensorflow/tv_graph_cnn/logs/signal_detection"),
         help='Logging directory'
+    )
+    parser.add_argument(
+        "--seed",
+        default=15,
+        help="Seed to create the random graph"
     )
     parser.add_argument(
         '--num_vertices',